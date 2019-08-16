1d057eab6d87ee66dc2794fc010a7c99f52155c1
Emily Morningstar <emorningstar@riis.com>
2019-08-16 14:03:25 -0400

    added check valid login

--- src/com/riis/JDBC.java
+++ src/com/riis/JDBC.java
@@ -8,58 +8,75 @@ import java.sql.Date;
 
 public class JDBC {
 
-    public static Connection connect(String url){
+    public static Connection connect(String url) {
         try (Connection connection = DriverManager.getConnection(url);) {
             return connection;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-   public static int newUser(String u,String f, String l, String email, int id, int roleID, String p ){
-        try{
+
+    public static int newUser(String u, String f, String l, String email, int id, int roleID, String p) {
+        try {
             Connection connection = DriverManager.getConnection(u);
             Statement stmt = connection.createStatement();
-            String q="USE PaidTimeOff INSERT INTO EMPLOYEES VALUES ('"+f+"', '"+l+"', '"+email+"', '"+roleID+"', '"+p+"');";
+            String q = "USE PaidTimeOff INSERT INTO EMPLOYEES VALUES ('" + f + "', '" + l + "', '" + email + "', '" + roleID + "', '" + p + "');";
             System.out.println(q);
             stmt.execute(q);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
         }
-       catch(Exception e) {
-           e.printStackTrace();
-           return -1;
-       }return 0;
+        return 0;
 
-   }
-    public static int CreateRequest(String u,String ds, String de, int eid){
-    Date dayS=Date.valueOf(ds);
-    Date dayE=Date.valueOf(de);
-    //status 2 is pending
-        try{
+    }
+
+    public static int CreateRequest(String u, String ds, String de, int eid) {
+        Date dayS = Date.valueOf(ds);
+        Date dayE = Date.valueOf(de);
+        //status 2 is pending
+        try {
             Connection connection = DriverManager.getConnection(u);
             Statement stmt = connection.createStatement();
-            String q="USE PaidTimeOff INSERT INTO Requests VALUES ('"+eid+"', '"+ds+"', '"+de+"',2);";
+            String q = "USE PaidTimeOff INSERT INTO Requests VALUES ('" + eid + "', '" + ds + "', '" + de + "',2);";
             System.out.println(q);
             stmt.execute(q);
 
             return 0;
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        return -1;}
+            return -1;
+        }
     }
-    public static int getEmployeeId(String u, String s){
-        try{
+
+    public static int getEmployeeId(String u, String s) {
+        try {
             Connection connection = DriverManager.getConnection(u);
             Statement stmt = connection.createStatement();
-            String q="USE PaidTimeOff SELECT id from Employees Where email='"+s+"';";
-            ResultSet rs=stmt.executeQuery(q);
+            String q = "USE PaidTimeOff SELECT id from Employees Where email='" + s + "';";
+            ResultSet rs = stmt.executeQuery(q);
             rs.next();
             return rs.getInt("id");
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            return -1;}
+            return -1;
+        }
+    }
+
+    public static String login(String u, String Username, String Password) {
+        try {
+            Connection connection = DriverManager.getConnection(u);
+            Statement stmt = connection.createStatement();
+            String q = "USE PaidTimeOff SELECT email from Employees Where email='" + Username + "'AND password='"+Password+"';";
+            System.out.println(q);
+            ResultSet rs = stmt.executeQuery(q);
+            rs.next();
+            return rs.getString("email");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- test/JDBCTest.java
+++ test/JDBCTest.java
@@ -3,12 +3,16 @@ import com.riis.JDBC;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.sql.Connection;
+
 public class JDBCTest {
-    JDBC jdbc=new JDBC();
+    JDBC jdbc = new JDBC();
+
     @Before
-    public void setup(){
+    public void setup() {
     }
+
     /*@Test
     public void connectTest() {
 
@@ -17,24 +21,36 @@ public class JDBCTest {
         Assert.assertNotNull(c);
     }*/
     //@Test
-    public void newUserTest(){
-        String url="jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
-        int i= JDBC.newUser(url,"name1","name2","email",3,2,"password");
-        Assert.assertEquals(0,i);
+    public void newUserTest() {
+        String url = "jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
+        int i = JDBC.newUser(url, "name1", "name2", "email", 3, 2, "password");
+        Assert.assertEquals(0, i);
     }
+
     //@Test
-    public void CreateRequestTest(){
-        String url="jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
+    public void CreateRequestTest() {
+        String url = "jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
 
         //yyyy-mm-dd
-       int i= JDBC.CreateRequest(url,"2000-1-1","2000-8-30",3);
-        Assert.assertEquals(0,i);
+        int i = JDBC.CreateRequest(url, "2000-1-1", "2000-8-30", 3);
+        Assert.assertEquals(0, i);
     }
+
     @Test
-    public void getEmployeeIdTest(){
-        String email="emorningstar@riis.com";
-        String url="jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
-        int i= JDBC.getEmployeeId(url, email);
-        Assert.assertEquals(3,i);
+    public void getEmployeeIdTest() {
+        String email = "emorningstar@riis.com";
+        String url = "jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
+        int i = JDBC.getEmployeeId(url, email);
+        Assert.assertEquals(3, i);
+    }
+
+    @Test
+    public void logInTestGood() {
+        String url = "jdbc:sqlserver://localhost;user=sa;password=reallyStrongPwd123";
+
+        String u = "emorningstar@riis.com";
+        String p = "password";
+        String s = JDBC.login(url, u, p);
+        Assert.assertEquals("emorningstar@riis.com", s);
     }
 }
